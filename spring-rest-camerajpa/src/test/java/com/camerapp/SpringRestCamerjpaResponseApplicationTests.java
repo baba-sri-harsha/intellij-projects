34deaf39dcbdeb3caa655c87e4a70ea72016baf2
baba-sri-harsha <91606108+baba-sri-harsha@users.noreply.github.com>
2022-04-28 10:19:57 +0530

    check

---
+++ spring-rest-camerajpa/src/test/java/com/camerapp/SpringRestCamerjpaResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCamerjpaResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
