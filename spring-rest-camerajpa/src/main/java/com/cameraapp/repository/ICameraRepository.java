0b634a7a63153470a7da5ae4525ea12372b6f30b
BabaSriHarshaErranki <baba.sriharsha@gmail.com>
2022-04-30 18:33:59 +0530

    a

--- spring-rest-camerajpa/src/main/java/com/cameraapp/repository/ICameraRepository.java
+++ spring-rest-camerajpa/src/main/java/com/cameraapp/repository/ICameraRepository.java
@@ -11,6 +11,8 @@ public interface ICameraRepository extends JpaRepository<Camera,Integer> {
 
     List<Camera>findAll();
     List<Camera> findByBrandAndPrice(String brand, double price);
+
+
     List<Camera>findByLensType(String lensType);
     List<Camera>findByCameraType(String cameraType);
     List<Camera>findByPrice(double price);
--- spring-rest-payingguest-exceptions_code/src/main/java/com/payingguest/model/Type.java
+++
@@ -1,5 +0,0 @@
-package com.payingguest.model;
-
-public enum Type {
-    MALE, FEMALE
-}
